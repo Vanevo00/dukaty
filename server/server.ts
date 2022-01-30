a26f3f040d61e4e63fc99667b860b71b53191d21
VojtaV <vanevo00@gmail.com>
2022-01-30 19:58:59 +0100

    basic graphql initiation

--- server/server.ts
+++ server/server.ts
@@ -1,9 +1,45 @@
 import express from 'express'
+import { ApolloServer, gql } from 'apollo-server-express'
+import http from "http"
 
-const port = 3666
+const port = 4001
 
-const app = express()
+const typeDefs = gql`
+  type Book {
+    title: String
+    author: String
+  }
 
-app.get('/', (req, res) => res.send('pes'))
+  type Query {
+    books: [Book]
+  }
+`;
 
-app.listen(port, () => console.log(`API listening on port ${port}`))
+const books = [
+    {
+      title: 'The Awakening',
+      author: 'Kate Chopin',
+    },
+    {
+      title: 'City of Glass',
+      author: 'Paul Auster',
+    },
+  ]
+
+const resolvers = {
+    Query: {
+        books: () => books,
+    },
+}
+
+const startApolloServer = async (typeDefs: any, resolvers: any) => {
+    const app = express()
+    const httpServer = http.createServer(app)
+    const server = new ApolloServer({ typeDefs, resolvers })
+    await server.start()
+    server.applyMiddleware({ app })
+    app.get('/', (req, res) => res.send('Welcome to dukaty api'))
+    app.listen(port, () => console.log(`API listening on port ${port}`))
+}
+
+startApolloServer(typeDefs, resolvers)
