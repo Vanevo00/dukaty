b503b82327fb8b94c746cec0ce4ceaf1415b5c09
VojtaV <vanevo00@gmail.com>
2022-02-15 10:02:35 +0100

    feat: add favicon

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -13,7 +13,8 @@ const OrderTracker: FC<AppProps> = ({ Component, pageProps }) => {
     <>
       <Head>
         <link href='./styles/styles.css' rel='stylesheet' />
-        <title>Svatováclavské dukáty</title>
+        <title>Dukáty.cz</title>
+        <link rel='shortcut icon' type='image/jpg' href='/favicon.ico' />
         <link rel='preconnect' href='https://fonts.googleapis.com' />
         <link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin />
         <link href='https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700&family=Poppins:wght@400;500;600;700&display=swap' rel='stylesheet' />
